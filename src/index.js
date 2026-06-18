005ec9f064bdd3dcc64914717a52340dd64cc75f
shwigy <floressj04@icloud.com>
2026-06-17 23:01:06 -0400

    Connected HTML forms to JavaScript file

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const form = document.getElementById("create-task-form");
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+  });
 });
